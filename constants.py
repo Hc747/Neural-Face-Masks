0b13a1e3580c3b8f3167eed5310acaaede160659
Hc747 <harrisoncole05@gmail.com>
2021-04-13 13:31:35 +1000

    - draw labels next to bounding boxes - make assertions being enabled configurable - make code more resilient to checked exceptions - update configuration and constants

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+from cv2 import FONT_HERSHEY_SIMPLEX
+
 IMAGE_SIZE: int = 224
 IMAGE_CHANNELS: int = 3
 
@@ -6,3 +8,6 @@ COLOUR_GREEN: tuple[int, int, int] = (0, 255, 0)
 COLOUR_BLUE: tuple[int, int, int] = (0, 0, 255)
 COLOUR_WHITE: tuple[int, int, int] = (255, 255, 255)
 COLOUR_BLACK: tuple[int, int, int] = (0, 0, 0)
+
+FONT_SCALE: float = 0.4
+FONT_FACE: int = FONT_HERSHEY_SIMPLEX
--- main.py
+++ main.py
@@ -13,13 +13,32 @@ from config import args, debug
 
 # TODO: logging
 # TODO: JIT compilation?
+# TODO: add more classes
+# TODO: relocate..?
+PREDICTION_MASKED: int = 0
+PREDICTION_UNMASKED: int = 1
+DISABLE_ASSERTS: bool = args.disable_assertions
 
 
-def get_class(probabilities: [float]):
+def evaluate_prediction(probabilities: [float]) -> tuple[int, float]:
+    """
+    :param probabilities: the prediction scores of each class
+    :return: a tuple containing the class indice and the 'confidence'
+    """
     values = np.asarray(probabilities)
     if values.shape[-1] > 1:
-        return values.argmax(axis=-1)
-    return (values > 0.5).astype('int32')
+        index = values.argmax(axis=-1)
+        confidence = values[index]
+    else:
+        # TODO: ensure confidence is correct...
+        score: float = values[0][0] * 100
+        if score > 50.0:
+            index = PREDICTION_UNMASKED
+            confidence = score
+        else:
+            index = PREDICTION_MASKED
+            confidence = 100 - score
+    return index, confidence
 
 
 def bind_lower(value: int, threshold: int) -> tuple[int, int]:
@@ -38,12 +57,6 @@ def bind(v0: int, v1: int, lower: int, upper: int) -> tuple[int, int]:
     return v0 + hi, v1 + lo
 
 
-# TODO: add more classes
-# TODO: relocate..?
-PREDICTION_MASKED: int = 0
-PREDICTION_UNMASKED: int = 1
-
-
 def pad(value: int, size: int = 3):
     return f'{value}'.ljust(3)
 
@@ -54,7 +67,7 @@ def process_frame(frame, face: FaceDetector, mask, frame_size: int, mask_input_s
     frame = np.asarray(resize_image(Image.fromarray(frame), frame_size))
 
     detections = face.detect(frame)
-    for detection in detections:
+    for idx, detection in enumerate(detections):
         # TODO: ensure crop dimensions are equal to face_size
         # TODO: ensure coordinates do not fall below 0 or above frame_size, and if so, slide across.
         # TODO: account for if face is too close (too large)...
@@ -65,16 +78,16 @@ def process_frame(frame, face: FaceDetector, mask, frame_size: int, mask_input_s
         # THRESHOLDS
         # top, left = 0,0
         # bottom, right = frame_size, frame_size
-        assert 0 < mask_input_size < frame_size, \
-            f'0 < {mask_input_size} < {frame_size} (0 < face < frame)'
+        assert DISABLE_ASSERTS or 0 < mask_input_size < frame_size, \
+            f'(0 < face < frame) = 0 < {mask_input_size} < {frame_size}'
 
         face_left, face_right = bind(face_left, face_right, 0, frame_size)
-        assert 0 <= face_left <= face_right <= frame_size, \
-            f'0 <= {face_left} <= {face_right} <= {frame_size} (0 <= left <= right <= frame)'
+        assert DISABLE_ASSERTS or 0 <= face_left <= face_right <= frame_size, \
+            f'(0 <= left <= right <= frame) = 0 <= {face_left} <= {face_right} <= {frame_size}'
 
         face_top, face_bottom = bind(face_top, face_bottom, 0, frame_size)
-        assert 0 <= face_top <= face_bottom <= frame_size, \
-            f'0 <= {face_top} <= {face_bottom} <= {frame_size} (0 <= top <= bottom <= frame)'
+        assert DISABLE_ASSERTS or 0 <= face_top <= face_bottom <= frame_size, \
+            f'(0 <= top <= bottom <= frame) = 0 <= {face_top} <= {face_bottom} <= {frame_size}'
 
         mask_dx = mask_input_size - face_width
         mask_dy = mask_input_size - face_height
@@ -102,12 +115,12 @@ def process_frame(frame, face: FaceDetector, mask, frame_size: int, mask_input_s
         offset_y = (mask_input_size - (crop_bottom - crop_top))
 
         crop_left, crop_right = bind(crop_left - offset_x, crop_right, 0, frame_size)
-        assert (crop_right - crop_left) == mask_input_size, \
-            f'(r - l == i) = ({crop_right} - {crop_left} == {mask_input_size}) = {crop_right - crop_left} == {mask_input_size}'
+        assert DISABLE_ASSERTS or (crop_right - crop_left) == mask_input_size, \
+            f'(right - left == mask) = ({crop_right} - {crop_left} == {mask_input_size}) = {crop_right - crop_left} == {mask_input_size}'
 
         crop_top, crop_bottom = bind(crop_top - offset_y, crop_bottom, 0, frame_size)
-        assert (crop_bottom - crop_top) == mask_input_size, \
-            f'(b - t == i) = ({crop_bottom} - {crop_top} == {mask_input_size}) = {crop_bottom - crop_top} == {mask_input_size}'
+        assert DISABLE_ASSERTS or (crop_bottom - crop_top) == mask_input_size, \
+            f'(bottom - top == mask) = ({crop_bottom} - {crop_top} == {mask_input_size}) = {crop_bottom - crop_top} == {mask_input_size}'
 
         lt_face_coordinates, rb_face_coordinates = (face_left, face_top), (face_right, face_bottom)
         lt_crop_coordinates, rb_crop_coordinates = (crop_left, crop_top), (crop_right - 1, crop_bottom - 1)
@@ -118,24 +131,36 @@ def process_frame(frame, face: FaceDetector, mask, frame_size: int, mask_input_s
 
         if width == mask_input_size and height == mask_input_size and channels == IMAGE_CHANNELS:
             crop_colour = COLOUR_BLUE
-            prediction = get_class(mask(np.reshape([face_image], (1, mask_input_size, mask_input_size, IMAGE_CHANNELS))))
+            prediction, _ = evaluate_prediction(mask(np.reshape([face_image], (1, mask_input_size, mask_input_size, IMAGE_CHANNELS))))
 
             if prediction == PREDICTION_MASKED:
                 face_colour = COLOUR_GREEN
+                category = 'Masked'
             elif prediction == PREDICTION_UNMASKED:
                 face_colour = COLOUR_RED
+                category = 'Unmasked'
             else:
                 face_colour = COLOUR_WHITE
+                category = 'Undetermined'
         else:
             prediction = None
+            category = 'Unknown'
             crop_colour = COLOUR_WHITE
             face_colour = COLOUR_WHITE
 
+        label = f'{idx + 1}: {category}'
+
+        # draw face bounding box
         cv2.rectangle(frame, lt_face_coordinates, rb_face_coordinates, face_colour, 1)
+        # draw classification label next to face bounding box
+        cv2.putText(frame, text=label, org=(lt_face_coordinates[0], lt_face_coordinates[1] - 10), fontFace=FONT_FACE, fontScale=FONT_SCALE, color=face_colour)
+        # draw crop bounding box
         cv2.rectangle(frame, lt_crop_coordinates, rb_crop_coordinates, crop_colour, 1)
+        # draw info label next to crop bounding box
+        cv2.putText(frame, text=f'Crop box: {idx + 1}', org=(lt_crop_coordinates[0], lt_crop_coordinates[1] - 10), fontFace=FONT_FACE, fontScale=FONT_SCALE, color=crop_colour)
 
         debug(f'face_image_boundary: (l,t,r,b){face_image_boundary}, face_image_shape: {np.shape(face_image)}')
-        debug(f'frame_size: {frame_size}, mask_input_size: {mask_input_size}, dx: {dx}, dy: {dy}')
+        debug(f'frame_size: {frame_size}, mask_input_size: {mask_input_size}, dx: {dx}, dy: {dy}, offset_x: {offset_x}, offset_y: {offset_y}')
         debug('---face---')
         debug(f'[L {pad(lt_face_coordinates[0])}, R {pad(rb_face_coordinates[0])}, W {pad(face_width)}]')
         debug(f'[T {pad(lt_face_coordinates[1])}, B {pad(rb_face_coordinates[1])}, H {pad(face_height)}]')
@@ -177,5 +202,5 @@ if __name__ == '__main__':
     faces: FaceDetector = get_face_detector(args)
     callback: FrameCallback = get_callback(args, faces, masks)
 
-    gui = GUI('Face the facts', width=args.width, height=args.height, callback=callback)
+    gui = GUI(title=args.title, width=args.width, height=args.height, callback=callback)
     gui.start()
