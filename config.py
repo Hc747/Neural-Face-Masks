4e8ed1020c368f3674fcccaab8f7af774f13f1f0
Hc747 <harrisoncole05@gmail.com>
2021-04-21 16:38:37 +1000

    - update configuration to account for default mask detector implementation

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import argparse
 import os
 from distutils.util import strtobool
+from constants import MASK_DETECTOR_ASHISH
 
 DEFAULT_TITLE: str = 'FTF: Face Mask Analyser'
 DEFAULT_IMAGE_SOURCE: str = 'video'
 DEFAULT_DETECTOR: str = 'realtime'
 DEFAULT_FACE_DETECTOR_PATH: str = os.path.abspath(os.path.join('.', 'models', 'face', 'mmod_human_face_detector.dat'))
-DEFAULT_MASK_DETECTOR_PATH: str = os.path.abspath(os.path.join('.', 'models', 'mask', 'checkpoint'))
+DEFAULT_MASK_DETECTOR: str = MASK_DETECTOR_ASHISH
 DEFAULT_WIDTH: int = 1024
 DEFAULT_HEIGHT: int = 720
 DEFAULT_FRAME_SIZE: int = 360
@@ -26,7 +27,7 @@ __parser = argparse.ArgumentParser()
 __parser.add_argument('--source', default=DEFAULT_IMAGE_SOURCE, help='Image source (video)')
 __parser.add_argument('--face_detector', default=DEFAULT_DETECTOR, help='Face detector (realtime or accurate)')
 __parser.add_argument('--face_detector_path', default=DEFAULT_FACE_DETECTOR_PATH, help='Face detector models path')
-__parser.add_argument('--mask_detector_path', default=DEFAULT_MASK_DETECTOR_PATH, help='Mask detector models path')
+__parser.add_argument('--mask_detector', default=DEFAULT_MASK_DETECTOR, help='Mask detector implementation')
 __parser.add_argument('--debug', type=__boolean, default=DEFAULT_DEBUG, help='Print debug statements (True/False)')
 __parser.add_argument('--disable_assertions', type=__boolean, default=DEFAULT_DISABLE_ASSERTIONS, help='Disable assertions at runtime (True/False)')
 __parser.add_argument('--title', default=DEFAULT_TITLE, help='Application GUI title')
