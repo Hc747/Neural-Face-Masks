d8398e710a3120603a475d64e42cfca454032fd8
Hc747 <harrisoncole05@gmail.com>
2021-05-04 01:53:33 +1000

    - add support for inception architecture

--- network/network_architecture.py
+++ network/network_architecture.py
@@ -81,9 +81,24 @@ class StandardResnetNetworkArchitecture(StandardNetworkArchitecture):
         return Model(inputs=base.input, outputs=classification)
 
 
+class StandardInceptionNetworkArchitecture(StandardNetworkArchitecture):
+    def __init__(self, shape, classes, inception):
+        super().__init__(shape, classes)
+        self.__inception = inception
+
+    def model(self):
+        base = self.__inception(weights='imagenet', include_top=False, input_shape=self.__shape, classes=self.__classes)
+        base.trainable = False
+
+        head = GlobalAveragePooling2D(name='avg_pool')(base.output)
+        classification = Dense(self.__classes, activation='softmax', name='predictions')(head)
+        return Model(inputs=base.input, outputs=classification)
+
+
 class ClassificationNetwork(NetworkArchitecture):
     __VGG_NETWORKS = [VGG16, VGG19]
     __RESNET_NETWORKS = [ResNet50, ResNet50V2, ResNet101, ResNet101V2, ResNet152, ResNet152V2]
+    __INCEPTION_NETWORKS = [InceptionV3]
 
     def __init__(self, base, shape, classes):
         self.base = base
@@ -96,6 +111,8 @@ class ClassificationNetwork(NetworkArchitecture):
             return StandardVGGNetworkArchitecture(shape, classes, vgg=network)
         elif network in ClassificationNetwork.__RESNET_NETWORKS:
             return StandardResnetNetworkArchitecture(shape, classes, resnet=network)
+        elif network in ClassificationNetwork.__INCEPTION_NETWORKS:
+            return StandardInceptionNetworkArchitecture(shape, classes, inception=network)
         else:
             raise ValueError(f'Unknown base classification network: {network}')
 
