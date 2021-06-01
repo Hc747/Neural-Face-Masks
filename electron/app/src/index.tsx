927534424401c67a893e041a9bd4c96dd6e680ec
Hc747 <harrisoncole05@gmail.com>
2021-06-02 00:16:28 +1000

    - remove electron folder

--- electron/app/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import {App} from './App';
-
-// import 'antd/dist/antd.css'
-import './App.global.css';
-
-render(<App />, document.getElementById('root'));
