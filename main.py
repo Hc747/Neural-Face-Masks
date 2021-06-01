4476a75185b8f9f49949fe917335088ebb2366f5
Hc747 <harrisoncole05@gmail.com>
2021-06-02 00:27:59 +1000

    - update main.py documentation

--- main.py
+++ main.py
@@ -10,26 +10,41 @@ from ui.callback.application_callback import ApplicationCallback
 from ui.callback.callback import FrameCallback
 from ui.gui import GUI
 
+"""
+The application entrypoint.
+"""
+
 if __name__ == '__main__':
     debug(lambda: f'Application configuration: {args}')
     debug(FaceDetectorProvider.version)
     debug(MaskDetectorProvider.version)
 
-    with FaceDetectorProvider.get_face_detector(FACE_DETECTOR_MEDIA_PIPE, confidence=0.5) as mp_face:
-        cnn_path: str = os.path.abspath(os.path.join('.', 'models', 'face', 'dlib', 'mmod_human_face_detector.dat'))
+    confidence: float = args.confidence
+    filename: str = os.path.abspath(os.path.join('.', 'models', 'face', 'dlib', 'mmod_human_face_detector.dat'))
+
+    # MediaPipe face detector needs to manage resources properly; therefore used in with block.
+    with FaceDetectorProvider.get_face_detector(FACE_DETECTOR_MEDIA_PIPE, confidence=confidence) as mp_face:
+        # define face detectors
         faces = {
             FACE_DETECTOR_MEDIA_PIPE: mp_face,
-            FACE_DETECTOR_CNN: FaceDetectorProvider.get_face_detector(FACE_DETECTOR_CNN, filename=cnn_path),
+            FACE_DETECTOR_CNN: FaceDetectorProvider.get_face_detector(FACE_DETECTOR_CNN, filename=filename),
             FACE_DETECTOR_SVM: FaceDetectorProvider.get_face_detector(FACE_DETECTOR_SVM)
         }
 
+        # define mask detectors
         masks = {
             MASK_DETECTOR_CABANI: MaskDetectorProvider.cabani(),
             MASK_DETECTOR_ASHISH: MaskDetectorProvider.ashish()
         }
 
+        # define application configuration
         configuration: ApplicationConfiguration = ApplicationConfiguration(args, faces=faces, masks=masks)
+
+        # define frame callback (application processing logic)
         callback: FrameCallback = ApplicationCallback(configuration)
 
-        gui = GUI(title=args.title, width=args.width, height=args.height, configuration=configuration, callback=callback)
+        title, width, height = args.title, args.width, args.height
+
+        # start the GUI
+        gui = GUI(title=title, width=width, height=height, configuration=configuration, callback=callback)
         gui.start()
