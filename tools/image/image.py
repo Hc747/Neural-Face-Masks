b7876f5377db18ef9c3e12210610e40bcc7e15fe
Hc747 <harrisoncole05@gmail.com>
2021-06-02 12:03:14 +1000

    - documentation for image.py

--- tools/image/image.py
+++ tools/image/image.py
@@ -1,10 +1,21 @@
 import cv2
 from skimage import io
 
+"""
+A module providing functionality to validate the format/integrity of images.
+"""
+
 __functions = [cv2.imread, io.imread]
 
 
 def is_valid_image(path) -> bool:
+    """
+    Determines whether or not an image at the path supplied is valid.
+    :param path:
+    The file system path specifying the location of the image.
+    :return:
+    True if the image is valid, False otherwise.
+    """
     for fn in __functions:
         try:
             image = fn(path)
@@ -15,6 +26,7 @@ def is_valid_image(path) -> bool:
         else:
             try:
                 shape = image.shape
+                # access a property of the image to ensure it loaded correctly.
             except:
                 return False
     return True
