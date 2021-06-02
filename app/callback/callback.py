3b2a70880955f743eb28cd68af07e0c96e901f65
Hc747 <harrisoncole05@gmail.com>
2021-06-02 18:39:56 +1000

    - documentation for callback.py

--- app/callback/callback.py
+++ app/callback/callback.py
@@ -1,14 +1,29 @@
 import abc
 from PIL.Image import Image
 
+"""
+A module exporting interfaces related to the processing of images / video frames.
+"""
+
 
 class FrameCallback(metaclass=abc.ABCMeta):
+    """
+    An interface defining the contract for converting a CV2 frame into a Pillow Image.
+    """
+
     @abc.abstractmethod
     def invoke(self, frame) -> Image:
+        """
+        An abstract method for performing some form of processing on an Image to be rendered.
+        """
         raise ValueError('FrameCallback#Invoke has not been implemented.')
 
 
 class LambdaFrameCallback(FrameCallback):
+    """
+    An implementation of the FrameCallback that allows for the use of a parameterised lambda function in place of
+    a concrete implementation/subclass of FrameCallback. Useful for prototyping, etc.
+    """
     def __init__(self, fn):
         self.__fn = fn
 
