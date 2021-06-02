dde3b9c91cff45e3fbb790633483edf04ea60194
Hc747 <harrisoncole05@gmail.com>
2021-06-02 12:10:20 +1000

    - documentation for common.py

--- tools/dataset/common.py
+++ tools/dataset/common.py
@@ -2,8 +2,31 @@ from keras_preprocessing.image import ImageDataGenerator
 from typing import Optional
 from constants import RANDOM_STATE
 
+"""
+A module providing common functionality for creating and manipulating datasets using the Keras ImageDataGenerator API.
+"""
 
-def flow(provider, directory, shape, subset=None, random_state=RANDOM_STATE, class_mode='categorical'):
+
+def flow(provider: ImageDataGenerator, directory: str, shape, subset=None, random_state=RANDOM_STATE, class_mode='categorical'):
+    """
+    Produces an iterable flow of images from an ImageDataGenerator in a manner consistent across different dataset implementations.
+
+    :param provider:
+    The ImageDataGenerator we are using to generate images.
+    :param directory:
+    The directory of the images on the file system.
+    :param shape:
+    The shape of the images.
+    :param subset:
+    The subset of this dataset. One of None, 'training' or 'validation'.
+    :param random_state:
+    The random state to use for reproducible invocations.
+    :param class_mode:
+    The classmode of the dataset. One of 'binary' or 'categorical'.
+
+    :return:
+    A directory iterator yielding Images.
+    """
     (w, h, channels) = shape
     color_mode: str = 'grayscale' if channels == 1 else 'rgb'
     return provider.flow_from_directory(
@@ -17,6 +40,15 @@ def flow(provider, directory, shape, subset=None, random_state=RANDOM_STATE, cla
 
 
 def get_standard_augmentor(split: Optional[float] = None) -> ImageDataGenerator:
+    """
+    Produces an ImageDataGenerator for augmenting training data in a manner that is consistent across different dataset implementations.
+
+    :param split:
+    The validation split (if dividing the dataaset into subsets).
+
+    :return:
+    An ImageDataGenerator to use for training a model.
+    """
     return ImageDataGenerator(
         rescale=1./255,
         rotation_range=45,
