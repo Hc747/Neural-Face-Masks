6a0460be81c0285dd575d2bf64e4abb1776c7af7
Hc747 <harrisoncole05@gmail.com>
2021-06-02 15:14:10 +1000

    - documentation for time_source.py

--- timing/time_source.py
+++ timing/time_source.py
@@ -1,15 +1,32 @@
 import time as timing
 
+"""
+A module exporting utility functions for measuring periods of time.
+"""
+
 
 class TimeSource:
+    """
+    An object that allows for the measuring of time at varying levels of precision (seconds, milliseconds, nanoseconds).
+    """
+
     @property
     def seconds(self) -> float:
+        """
+        The current timestamp in seconds since the unix epoch
+        """
         return timing.time()
 
     @property
     def millis(self) -> int:
+        """
+        The current timestamp in milliseconds since the unix epoch
+        """
         return int(self.seconds * 1000)
 
     @property
     def nanos(self) -> int:
+        """
+        The current timestamp in nanoseconds since the unix epoch
+        """
         return timing.time_ns()
