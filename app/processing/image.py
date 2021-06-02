a6828a8f01e21bffa4eef0abb95c4d96613158b6
Hc747 <harrisoncole05@gmail.com>
2021-06-02 18:37:36 +1000

    - documentation for image.py

--- app/processing/image.py
+++ app/processing/image.py
@@ -2,8 +2,15 @@ import cv2
 import numpy as np
 from typing import Optional, Tuple
 
+"""
+A module exporting utility functions for image processing.
+"""
+
 
 def centered_crop(img, size, interpolation=cv2.INTER_AREA):
+    """
+    Crops an image into a square of the specified size originating from the center.
+    """
     h, w = img.shape[:2]
     min_halved = int(np.amin([h, w]) / 2)
     h = int(h / 2)
@@ -14,10 +21,16 @@ def centered_crop(img, size, interpolation=cv2.INTER_AREA):
 
 
 def resize(frame, size, interpolation=cv2.INTER_AREA):
+    """
+    Resizes an image to the specified size.
+    """
     return cv2.resize(frame, size, interpolation=interpolation)
 
 
 def rescale(frame, scale: Optional[float], interpolation=cv2.INTER_AREA):
+    """
+    Rescales an image by the specified scale.
+    """
     if scale is None:
         return frame, frame, 1.0
     ratio: float = 1.0 / scale
@@ -27,10 +40,16 @@ def rescale(frame, scale: Optional[float], interpolation=cv2.INTER_AREA):
 
 
 def translate_scale(x, scale: float):
+    """
+    Translates coordinates (x) from one scale to the specified scale.
+    """
     return (np.asarray(x) * scale).astype(int)
 
 
 def adjust_bounding_box(left: int, top: int, right: int, bottom: int, offset: int) -> Tuple[int, int, int, int, int, int]:
+    """
+    Applies padding to the coordinates of a bounding box.
+    """
     left, top = left - offset, top - offset
     right, bottom = right + offset, bottom + offset
     width, height = right - left, bottom - top
